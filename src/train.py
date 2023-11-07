733f3f1c2bc71ad126efbad667901cff245662fa
Timofey <timofejbakurov@gmall.com>
2023-11-07 19:08:58 +0700

    Линейная регрессия натренирована через пайплайн

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from skimage.io import imread_collection
 from skimage.transform import resize
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier, LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 
@@ -38,8 +38,8 @@ def load_data(data_path):
 def main(repo_path):
     train_csv_path = repo_path / "data/prepared/train.csv"
     train_data, labels = load_data(train_csv_path)
-    rf = RandomForestClassifier()                  
-    trained_model = rf.fit(train_data, labels)
+    lr = LogisticRegression(max_iter=100)         
+    trained_model = lr.fit(train_data, labels)
     dump(trained_model, repo_path / "model/model.joblib")
 
 
